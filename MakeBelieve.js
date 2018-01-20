ff548bc62cb2a5f15e636e5d526ad886f20110a1
Arnar Þórðarson <arnart08@ru.is>
2018-01-20 12:39:49 +0000

    worked on ajax function

--- MakeBelieve.js
+++ MakeBelieve.js
@@ -1,12 +1,14 @@
 (function() {
 
+	
+
 	// function that returns if an object is the empty object.
 	function isEmptyObject(object) {
-		if (Object.keys(this).length == 0){
+		if (Object.keys(object).length == 0){
 			return true;
 		}
 		return false;
-	}
+	};
 
 	function MyElem(elem, length) {
 		this.elem = elem;
@@ -21,13 +23,15 @@
 		return {};
 	};
 
+	// Returns the parent elements of the elements in this that match the 
+	// query string or if no string given
 	MyElem.prototype.parent = function(query) {
 
-		parents = [];
+		
 		if(isEmptyObject(this)) {
 			return {};
 		}
-
+		parents = [];
 		for(var i = 0; i < this.length; i++) {
 			parentElement = this.elem[i].parentElement;
 			if(query) {
@@ -49,6 +53,8 @@
 
 	};
 
+	// returns the grandparents of the elements in this that match the 
+	// query string or if no string given
 	MyElem.prototype.grandParent = function(query) {
 		grandParents = [];
 		if(isEmptyObject(this)) {
@@ -81,7 +87,8 @@
 		return new MyElem(grandParents, grandParents.length);
 	};
 
-
+	// returns first ancestor of the elements in this that match the 
+	// query string or if no string given
 	MyElem.prototype.ancestor = function(query) {
 		ancestors = [];
 		if(isEmptyObject(this)) {
@@ -97,11 +104,15 @@
 								ancestor = ancestor.parentElement;
 							}
 							if(ancestor.matches(query)) {
-								ancestors.push(ancestor);
+								if(!ancestors.includes(ancestor)) {
+									ancestors.push(ancestor);
+								}
 							}
 						}
 						else {
-							ancestors.push(ancestor);
+							if(!ancestors.includes(ancestor)) {
+								ancestors.push(ancestor);
+							}
 						}
 					}
 				}
@@ -110,6 +121,7 @@
 		return new MyElem(ancestors, ancestors.length);
 	};
 
+	// insert text into the elements in this.
 	MyElem.prototype.insertText = function(text) {
 		if(isEmptyObject(this)) {
 			return {};
@@ -121,6 +133,8 @@
 		}
 	};
 
+	// attach an onClick event to the elements in this that will execute 
+	// the func function when triggered
 	MyElem.prototype.onClick = function(func) {
 		if(isEmptyObject(this)) {
 			return {};
@@ -131,7 +145,7 @@
 		}
 	};
 
-	
+	// append an element to the elements in this as it's last child
 	MyElem.prototype.append = function(newElement) {
 		if(typeof newElement == "string") {
 			for(var element of this.elem) {
@@ -145,6 +159,7 @@
 		}
 	};
 
+	// prepend an element to the elements in this as it's first child
 	MyElem.prototype.prepend = function(newElement) {
 		if(typeof newElement == "string") {
 			for(var element of this.elem) {
@@ -163,10 +178,12 @@
 		}
 	};
 
+	// delete the elements in this
 	MyElem.prototype.delete = function() {
 		if(isEmptyObject(this)) {
 			return {};
 		}
+
 		for(var element of this.elem) {
 			if(element.parentElement) {
 				element.parentElement.removeChild(element);
@@ -174,19 +191,51 @@
 		}
 	};
 
-	MyElem.prototype.ajax = function(options) {
-		var method = options.method || 'GET';
+	// perform an ajax http request
+	makeBelieve.ajax = function(options) {
+		var tempMethod = options.method;
+		var method;
+		if(typeof tempMethod === 'string') {
+			tempMethod = tempMethod.toUpperCase();
+			if(tempMethod === 'POST' || tempMethod === 'PUT' || tempMethod === 'OPTIONS' ||
+				tempMethod === 'HEAD' || tempMethod === 'DELETE') {
+				method = tempMethod;
+			}
+		}
+		method = method || 'GET';
 		var request = new XMLHttpRequest();
 		request.open(method, options.url);
+		for(var header of options.headers) {
+			request.setRequestHeader(Object.keys(header)[0], header[Object.keys(header)]);
+			console.log(Object.keys(header)[0], typeof(Object.keys(header)[0]));
+			console.log(header[Object.keys(header)], typeof(header[Object.keys(header)]));
+			//request.setRequestHeader();
+		}
+		
 		request.onreadystatechange = function() {
-			if (request.readyState === XMLHttpRequest.DONE && request.status === 200) {
-				console.log(request.responseText);
+			if (request.readyState === XMLHttpRequest.DONE ) {
+				if(request.status >= 200 && request.status < 300) {
+					if(typeof options.success === 'function') {
+						options.success(request.responseText);
+					}
+					
+				}
+				else if(request.status >= 400) {
+					if(typeof options.fail === 'function') {
+						options.fail(request.responseText);
+					}
+					
+				}
 			}
 		};
 
+		if(typeof options.beforeSend === 'function') {
+			options.beforeSend(request);
+		}
 		request.send(JSON.stringify(options.data));
 	};
 
+	// set the css atribute to the value given on all elements in this.
 	MyElem.prototype.css = function(attribute, value) {
 		if(isEmptyObject(this)) {
 			return {};
@@ -194,9 +243,9 @@
 		for(var element of this.elem) {
 			element.style[attribute] = value;
 		}
-		console.log(this);
 	};
 
+	// toggle a class on the elements in this.
 	MyElem.prototype.toggleClass = function(newClass) {
 		if(isEmptyObject(this)) {
 			return {};
@@ -206,6 +255,7 @@
 		}
 	};
 
+	// add a sumbit event listener to the elements in this.
 	MyElem.prototype.onSubmit = function(func) {
 	if(isEmptyObject(this)) {
 			return {};
@@ -215,6 +265,7 @@
 		}
 	};
 
+	// add an event lister for the onInput event to the elements in this.
 	MyElem.prototype.onInput = function(func) {
 		if(isEmptyObject(this)) {
 			return {};
